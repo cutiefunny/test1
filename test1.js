8a299eab3feda8148e8d069c1cd70f9b8d3d1c09
cutiefunny <cutiefunny@gmail.com>
2021-07-19 17:26:53 +0900

    mongoDB test

--- test1.js
+++ test1.js
@@ -4,21 +4,21 @@ var btn = document.getElementById('btn1');
 var txt = document.querySelector('p');
 var label = document.querySelector('label');
 
-import mongoose from "mongoose";
+//import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://cutiefunny:ghks1015@macrodb.srkli.mongodb.net/macroDB?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useFindAndModify: true
-});
+// mongoose.connect("mongodb+srv://cutiefunny:ghks1015@macrodb.srkli.mongodb.net/macroDB?retryWrites=true&w=majority", {
+//   useNewUrlParser: true,
+//   useFindAndModify: true
+// });
 
 //MongoDB와의 연결을 db로 저장
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅Connected to DB");
-const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
+// const handleOpen = () => console.log("✅Connected to DB");
+// const handleError = () => console.log(`❌ Error on DB Connection:${error}`);
 
-db.once("open", handleOpen);
-db.on("eroor", handleError);
+// db.once("open", handleOpen);
+// db.on("eroor", handleError);
 
 //btn.addEventListener('click', updateBtn);
 
